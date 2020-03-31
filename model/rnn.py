Fixed relation masking

model/rnn.py
@@ -115,15 +115,13 @@ class RelationModel(object):
         probs = F.softmax(logits, dim=1).data.cpu().numpy().tolist()
 
         if self.opt['relation_masking']:
-            desired_relation_logits = logits[:, labels]
-            # Need to negate mask because masked_fill replaces values where mask=1 with substitute.
-            # In our case, mask=1 -> known relation, mask=0 -> distraction relation (to be removed)
-            relation_masks = inputs['supplemental']['relation_masks'][0].eq(0)
-            logits = logits.masked_fill(relation_masks, -np.inf)
-            # Place correct relation probs back into probability matrix
-            logits[:, labels] = desired_relation_logits
+            relation_masks = inputs['supplemental']['relation_masks'][0].data.cpu().numpy()
+            logits = logits.data.cpu().numpy()
+            target_values = logits[np.arange(0, len(logits)), labels]
+            logits[relation_masks == 1] = -np.inf
+            logits[np.arange(0, len(logits)), labels] = target_values
 
-        predictions = np.argmax(logits.data.cpu().numpy(), axis=1).tolist()
+        predictions = np.argmax(logits, axis=1).tolist()
         if unsort:
             _, predictions, probs = [list(t) for t in zip(*sorted(zip(orig_idx,\
                     predictions, probs)))]