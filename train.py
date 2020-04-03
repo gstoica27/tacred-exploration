7ea39fb0cae59a997a259838150919284f4a0913
gstoica27 <gstoica27@gmail.com>
2020-04-03 00:01:07 -0700

    fixed bug

--- train.py
+++ train.py
@@ -156,7 +156,7 @@ for epoch in range(1, opt['num_epoch']+1):
             print(print_info + loss_prints)
 
     # update lambda if needed
-    if epoch % opt['kg_loss']['lambda_update_gap'] == 0:
+    if opt['kg_loss'] is not None and epoch % opt['kg_loss']['lambda_update_gap'] == 0:
         model.update_lambda_term()
 
     print("Evaluating on train set...")
