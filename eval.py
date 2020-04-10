33abe0a78a2eb07c89da1c379114575d09b4c10c
gstoica27 <gstoica27@gmail.com>
2020-04-10 15:59:51 -0700

    minimal necessary updates to eval

--- eval.py
+++ eval.py
@@ -14,13 +14,20 @@ from data.loader import DataLoader
 from model.rnn import RelationModel
 from utils import torch_utils, scorer, constant, helper
 from utils.vocab import Vocab
+import numpy as np
+import json
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('model_dir', type=str, help='Directory of the model.')
+parser.add_argument('--model_dir', type=str, help='Directory of the model.',
+                    default='/usr0/home/gis/research/tacred-exploration/saved_models/PA-LSTM-TACRED')
 parser.add_argument('--model', type=str, default='best_model.pt', help='Name of the model file.')
-parser.add_argument('--data_dir', type=str, default='dataset/tacred')
+parser.add_argument('--data_dir', type=str, default='/usr0/home/gis/data/tacred/data/json')
+parser.add_argument('--vocab_dir', type=str, default='/usr0/home/gis/data/tacred/data/vocab')
 parser.add_argument('--dataset', type=str, default='test', help="Evaluate on dev or test.")
-parser.add_argument('--out', type=str, default='', help="Save model predictions to this dir.")
+parser.add_argument('--test_save_dir', type=str,
+                    default='/usr0/home/gis/research/tacred-exploration/tacred_test_performances',
+                    help='Test save directory')
 
 parser.add_argument('--seed', type=int, default=1234)
 parser.add_argument('--cuda', type=bool, default=torch.cuda.is_available())
@@ -60,12 +67,33 @@ id2label = dict([(v,k) for k,v in constant.LABEL_TO_ID.items()])
 
 predictions = []
 all_probs = []
+data = []
+correct_predictions = []
+incorrect_predictions = []
 for i, b in enumerate(batch):
     preds, probs, _ = model.predict(b)
     predictions += preds
     all_probs += probs
+
 predictions = [id2label[p] for p in predictions]
 p, r, f1 = scorer.score(batch.gold(), predictions, verbose=True)
+
+is_correct = np.array(predictions) == np.array(batch.gold())
+is_incorrect = np.array(predictions) != np.array(batch.gold())
+correct_predictions = np.arange(len(predictions))[is_correct]
+incorrect_predictions = np.arange(len(predictions))[is_incorrect]
+with open(data_file, 'rb') as handle:
+    data = np.array(json.load(handle))
+correct_data = data[correct_predictions].tolist()
+incorrect_data = data[incorrect_predictions].tolist()
+
+save_dir = os.path.join(opt['test_save_dir'], opt['id'], 'correctness')
+os.makedirs(save_dir, exist_ok=True)
+with open(os.path.join(save_dir, 'correct_data.pkl'), 'wb') as handle:
+    pickle.dump(correct_data, handle)
+with open(os.path.join(save_dir, 'incorrect_data.pkl'), 'wb') as handle:
+    pickle.dump(incorrect_data, handle)
+
 
 # save probability scores
 if len(args.out) > 0:
